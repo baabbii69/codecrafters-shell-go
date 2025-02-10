bcc20060891fc0e6e42a9449ea83bda3351dac7d
baabbii <yohanesalemu0069@gmail.com>
2025-02-10 09:32:20 +0300

    exit

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,7 +11,6 @@ var _ = fmt.Fprint
 
 func main() {
 	for {
-
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
@@ -19,6 +18,9 @@ func main() {
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
+		}
+		if command == "exit 0\n" {
+			os.Exit(0)
 		}
 		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
