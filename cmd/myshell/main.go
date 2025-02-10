474bfd5fda618c8b2e5fb8d571e3e14b9d4261c0
baabbii <yohanesalemu0069@gmail.com>
2025-02-10 11:22:32 +0300

    type builtin

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,33 +7,87 @@ import (
 	"strings"
 )
 
+var builtins map[string]func([]string)
+
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
 func main() {
+	builtins = map[string]func([]string){
+		"echo": handleEcho,
+		"exit": handleExit,
+		"type": handleType,
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		// Wait for user input
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		if !scanner.Scan() {
+			break
+		}
+
+		command := scanner.Text()
+		words := strings.Fields(command)
+		if len(words) == 0 {
+			continue
+		}
+		cmd := words[0]
+		arg := words[1:]
+		if handler, exists := builtins[cmd]; exists {
+			handler(arg)
+		} else {
+			fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd)
+		}
+
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
-		word := strings.Fields(command)
-		var words []string
-		for _, w := range word {
-			words = append(words, w)
-		}
-
-		switch words[0] {
-		case "echo":
-			fmt.Fprintf(os.Stdout, "%s\n", strings.Join(words[1:], " "))
-		case "exit":
-			os.Exit(0)
-		default:
-			fmt.Fprintf(os.Stderr, "%s: command not found\n", command[:len(command)-1])
-		}
-
 	}
 }
+func handleEcho(args []string) {
+	fmt.Println(strings.Join(args, " "))
+}
+
+func handleExit(args []string) {
+	os.Exit(0)
+}
+
+func handleType(args []string) {
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, "type: missing argument")
+		return
+	}
+
+	cmd := args[0]
+	if _, exists := builtins[cmd]; exists {
+		fmt.Printf("%s is a shell builtin\n", cmd)
+	} else {
+		fmt.Printf("%s: not found\n", cmd)
+	}
+}
+
+//fmt.Fprint(os.Stdout, "$ ")
+//
+//// Wait for user input
+//command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+//if err != nil {
+//	fmt.Fprintln(os.Stderr, "Error reading input:", err)
+//	os.Exit(1)
+//}
+//word := strings.Fields(command)
+//var words []string
+//for _, w := range word {
+//	words = append(words, w)
+//}
+//
+//switch words[0] {
+//case "echo":
+//	fmt.Fprintf(os.Stdout, "%s\n", strings.Join(words[1:], " "))
+//case "exit":
+//	os.Exit(0)
+//default:
+//	fmt.Fprintf(os.Stderr, "%s: command not found\n", command[:len(command)-1])
+//}
